b0ab6f41ef740b9e4711bd2bcc1805797c0eaa5e
ifozest <aemaeth4@gmail.com>
2013-09-25 16:40:41 +0300

    remake menu items as links

--- public/js/view/menuView.js
+++ public/js/view/menuView.js
@@ -2,27 +2,17 @@ define([
   'underscore',
   'backbone',
   'text!templates/main/menu/menu.html'
-], function(_,Backbone,menuTemplate) {
+], function (_, Backbone, menuTemplate) {
 
   var MenuView = Backbone.View.extend({
     tagName: 'div',
     className: 'menuBlock',
-    events: {
-      'click li#showNews' : 'showNewsEvent',
-      'click li#createNews' : 'createNewsEvent'
-    },
-    initialize : function(){
+    initialize: function () {
       this.template = _.template(menuTemplate);
     },
-    render : function() {
+    render: function () {
       this.$el.html(this.template);
       return this;
-    },
-    showNewsEvent : function() {
-      this.goTo('home');
-    },
-    createNewsEvent : function() {
-      this.goTo('createNews');
     }
   });
   return MenuView;
